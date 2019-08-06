6375003a053837d0d18f924a4d1d6603eb0e55ef
xiaominglol <474345633@qq.com>
2019-08-06 17:54:51 +0800

    代码大调整

--- boot-framework-example/boot-web-example/src/main/java/com/gemini/boot/framework/web/controller/TestController.java
+++ boot-framework-example/boot-web-example/src/main/java/com/gemini/boot/framework/web/controller/TestController.java
@@ -1,24 +1,24 @@
-package com.gemini.boot.framework.web.controller;
-
-import com.gemini.boot.framework.web.entity.Test;
-import com.gemini.boot.framework.web.mvc.model.Message;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class TestController {
-    @Value("${cloud.id}")
-    private String cloudId;
-
-    @Value("${cloud.name}")
-    private String cloudName;
-
-    @GetMapping("/test")
-    public Message test() {
-        Test test = new Test();
-        test.setCloudId(cloudId);
-        test.setCloudName(cloudName);
-        return Message.success(test);
-    }
-}
+//package com.gemini.boot.framework.web.controller;
+//
+//import com.gemini.boot.framework.web.entity.Test;
+//import com.gemini.boot.framework.web.mvc.model.Message;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class TestController {
+//    @Value("${cloud.id}")
+//    private String cloudId;
+//
+//    @Value("${cloud.name}")
+//    private String cloudName;
+//
+//    @GetMapping("/test")
+//    public Message test() {
+//        Test test = new Test();
+//        test.setCloudId(cloudId);
+//        test.setCloudName(cloudName);
+//        return Message.success(test);
+//    }
+//}
